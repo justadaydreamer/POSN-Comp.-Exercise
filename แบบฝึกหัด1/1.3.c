4c14dae6e8bb97f9715102f0e24ceb4fc75ddc4a
Thanatud Boonyaanuchit <73384731+justadaydreamer@users.noreply.github.com>
2022-11-02 18:37:13 +0700

    Add files via upload

---
+++ แบบฝึกหัด1/1.3.c
@@ -0,0 +1,14 @@
+//แบบฝึกหัดที่ 1 ข้อ 1.3
+
+#include <stdio.h>
+
+int main(){
+    int inc, value;
+    inc = 20;
+
+    value = inc++;
+    printf("%d\n", value);
+
+    value = ++inc;
+    printf("%d\n", value);
+}
