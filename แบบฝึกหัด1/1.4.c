ede50e6733b0ff5a030230879f94f0b7a6175b04
Thanatud Boonyaanuchit <73384731+justadaydreamer@users.noreply.github.com>
2022-11-03 20:31:28 +0700

    Add files via upload

---
+++ แบบฝึกหัด1/1.4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float radius, area , circumference;
+    #define PI 3.14
+
+    printf("Radius :");
+    scanf("%f", &radius);
+
+    area = PI*radius*radius;
+    circumference = 2*PI*radius;
+    
+    printf("The area and circumference : \n");
+    printf("%f\t%f\n", area,circumference);
+}
